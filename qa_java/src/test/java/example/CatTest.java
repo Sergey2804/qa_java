ded9accfd3327e46cce84c21940ded6061c1a551
Sergey2804 <sergeywiprqoiy@gmail.com>
2025-09-29 13:49:28 +0300

    Add files via upload

---
+++ qa_java/src/test/java/example/CatTest.java
@@ -0,0 +1,37 @@
+package example;
+
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void testGetSound() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
+
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+
+        List<String> actualFood = cat.getFood();
+        assertEquals(expectedFood, actualFood);
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+    }
+}
